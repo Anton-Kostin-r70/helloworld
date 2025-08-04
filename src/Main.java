149df219558a61e1de013fdf88c1afecb252852f
Anton Kostin <kostinmesbox@mail.ru>
2025-08-04 22:15:39 +0700

    Added the HW-5

--- src/Main.java
+++ src/Main.java
@@ -125,13 +125,13 @@ public class Main {
         float christinSalary = 76230f;
         float surchargePercentage = 10f;
         float seniorityCoefficient = surchargePercentage / 100;
-        byte year = 12;
+        byte years = 12;
         float newMashaSalary = mashaSalary * ( 1 + seniorityCoefficient );
         float newDenisSalary = denisSalary * ( 1 + seniorityCoefficient );
         float newChristinSalary = christinSalary * ( 1 + seniorityCoefficient );
-        float mashaAnnualIncome = mashaSalary * year;
-        float denisAnnualIncome = denisSalary * year;
-        float christinAnnualIncome = christinSalary * year;
+        float mashaAnnualIncome = mashaSalary * years;
+        float denisAnnualIncome = denisSalary * years;
+        float christinAnnualIncome = christinSalary * years;
         System.out.println ( "Маша теперь получает " + newMashaSalary + " рублей."
                 + " Годовой доход вырос на " + mashaAnnualIncome * seniorityCoefficient + " рублей");
         System.out.println ( "Денис теперь получает " + newDenisSalary + " рублей."
@@ -217,5 +217,95 @@ public class Main {
             max = three;
         }
         System.out.println( max );
+        // the HOMEWORK 5
+        byte clientOS = 0;
+        switch( clientOS ) {
+            case 0:
+                System.out.println( "Установите версию приложения для iOS по ссылке" );
+                break;
+            case 1:
+                System.out.println( "Установите версию приложения для Android по ссылке" );
+                break;
+            default:
+                System.out.println( "Операционная система не определена" );
+        }
+        short clientDeviceYear = 2015;
+        switch( clientOS ) {
+            case 0: {
+                if ( clientDeviceYear > 2015 )
+                    System.out.println( "Установите версию приложения для iOS по ссылке" );
+                else
+                    System.out.println( "Установите облегченную версию приложения для iOS по ссылке" );
+                break;
+            }
+            case 1: {
+                if ( clientDeviceYear > 2015 )
+                    System.out.println( "Установите версию приложения для Android по ссылке" );
+                else
+                    System.out.println( "Установите облегченную версию приложения для Android по ссылке" );
+                break;
+            }
+            default:
+                System.out.println( "Операционная система не определена" );
+        }
+        short year = 2021;
+        if ( ( year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ) && year > 1584 )
+            System.out.println( year + " год является високосным" );
+        else
+            System.out.println( year + " год не является високосным" );
+        short deliveryDistance = 95;
+        byte days = 0;
+        if ( deliveryDistance < 20 ) {
+            days = 1;
+        }
+        else if ( deliveryDistance >= 20 && deliveryDistance < 60 ) {
+            days = 2;
+        }
+        else if ( deliveryDistance >= 60 && deliveryDistance <= 100 ) {
+            days = 3;
+        }
+        else if ( deliveryDistance > 100 ) {
+            System.out.println( "Доставки нет" );
+        }
+        switch ( days ) {
+            case 1:
+                System.out.println( "Потребуется дней: " + days );
+                break;
+            case 2:
+                System.out.println( "Потребуется дней: " + days );
+                break;
+            case 3:
+                System.out.println( "Потребуется дней: " + days );
+                break;
+            default:
+                System.out.println( "Доставки нет" );
+        }
+        byte monthNumber = 12;
+        if ( monthNumber <= 12 ) {
+            switch ( monthNumber ) {
+                case 1:
+                case 2:
+                case 12:
+                    System.out.println( "Зима" );
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    System.out.println( "Весна" );
+                 break;
+                case 6:
+                case 7:
+                case 8:
+                    System.out.println( "Лето" );
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    System.out.println( "Осень" );
+                    break;
+                default:
+                    System.out.println( "Такого сезона нет" );
+            }
+        }
     }
 }
